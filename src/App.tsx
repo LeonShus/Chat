e4a2e43b74a8a62a5b593198d3f08fcbdc2057d0
LeonShus <leonshustov1995@gmail.com>
2022-01-31 14:07:55 +0300

    try to create WebSocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    useEffect(() => {
+        const socket = new WebSocket("wss://social-network.samuraijs.com/handlers/chatHandler.ashx")
+
+        //@ts-ignore
+        window.socket = socket
+
+        socket.onmessage = (event) => {
+            console.log("Data" + event.data)
+        }
+    }, [])
+
+    return (
+        <div className="App">
+            Hello
+        </div>
+    )
 }
 
 export default App;
